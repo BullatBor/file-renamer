466fbf26806a956c3c1185221a2dbd9aaf9b8fad
Berezhnev Vladimir <vova21473@gmail.com>
2023-07-09 13:20:22 +1000

    Delete react-dnd and replace with own functions

--- src/renderer/layout/OverviewItemList/OverviewItemList.tsx
+++ src/renderer/layout/OverviewItemList/OverviewItemList.tsx
@@ -3,9 +3,14 @@ import styles from './OverviewItemList.module.css'
 import FileItems from '../../pages/FilePage/FileItems/FileItems'
 import NamingItems from '../../pages/NamingPage/NamingItems/NamingItems'
 
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 const OverviewItemList = () => {
     const location = useLocation()
     const handleDrop = (files: any) => {
+        // @ts-ignore
+        inputRef.current.click()
         // Handle dropped files here
         console.log(files)
     }
--- src/renderer/pages/FilePage/FileItems/FileItems.tsx
+++ src/renderer/pages/FilePage/FileItems/FileItems.tsx
@@ -1,5 +1,5 @@
 import Item from '../../../components/Item/Item'
-import { useDrop } from 'react-dnd'
+import { useState, useRef } from 'react'
 
 const mockFiles = [
     { id: 1, name: 'Capybara.png', fileSize: '10MB' },
@@ -7,27 +7,77 @@ const mockFiles = [
     { id: 3, name: 'Giraffe.gif', fileSize: '2MB' },
 ]
 
-const FileItems = ({ onDrop }: any) => {
-    const [{ isOver }, drop] = useDrop({
-        accept: 'file',
-        drop: (item: any) => onDrop(item.files),
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-        }),
-    })
+function handleFile(files: any) {
+    alert('Number of files: ' + files.length)
+}
+
+const FileItems = ({ onButtonClick }: any) => {
+    // drag state
+    const [dragActive, setDragActive] = useState(false)
+    // ref
+    const inputRef = useRef(null)
+
+    // handle drag events
+    const handleDrag = function (e: any) {
+        e.preventDefault()
+        e.stopPropagation()
+        if (e.type === 'dragenter' || e.type === 'dragover') {
+            setDragActive(true)
+        } else if (e.type === 'dragleave') {
+            setDragActive(false)
+        }
+    }
+
+    // triggers when file is dropped
+    const handleDrop = function (e: any) {
+        e.preventDefault()
+        e.stopPropagation()
+        setDragActive(false)
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            handleFile(e.dataTransfer.files)
+        }
+    }
+
+    // triggers when file is selected with click
+    const handleChange = function (e: any) {
+        e.preventDefault()
+        if (e.target.files && e.target.files[0]) {
+            handleFile(e.target.files)
+        }
+    }
 
     return (
-        <div
-            ref={drop}
-            style={{
-                border: `2px dashed ${isOver ? 'green' : 'gray'}`,
-                padding: '10px',
-                margin: '10px',
-                minHeight: '100px',
-            }}
-        >
-            Drop files here
-        </div>
+        <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+            <input
+                ref={inputRef}
+                type="file"
+                id="input-file-upload"
+                multiple={true}
+                onChange={handleChange}
+            />
+            <label
+                id="label-file-upload"
+                htmlFor="input-file-upload"
+                className={dragActive ? 'drag-active' : ''}
+            >
+                <div>
+                    <p>Drag and drop your file here or</p>
+                    <button className="upload-button" onClick={onButtonClick}>
+                        Upload a file
+                    </button>
+                </div>
+            </label>
+            {dragActive && (
+                <div
+                    id="drag-file-element"
+                    onDragEnter={handleDrag}
+                    onDragLeave={handleDrag}
+                    onDragOver={handleDrag}
+                    onDrop={handleDrop}
+                    style={{ height: '100px', width: '100px', backgroundColor: 'purple' }}
+                ></div>
+            )}
+        </form>
     )
 }
 
